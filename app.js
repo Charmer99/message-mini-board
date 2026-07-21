4bd4b8fffbc9b66a52ebe1775d42f85681278303
Charmer99 <Calebkyamaweb@gmail.com>
2026-07-21 16:52:43 +0300

    Prep to deploy

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require("express");
 const app = express();
-const path = require("path")
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
 
 
 const indexRouter = require("./routes/indexRouter");
 
-app.set("view engine", "ejs")
-app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter)
+app.use("/", indexRouter);
 
- app.listen(3000, ()=> {
-    console.log("Server running");
-    
- })
+app.listen(PORT, () => {
+   console.log(`Server running on port ${PORT}`);
+});
